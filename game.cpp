e9131a8cd2fb46188eb5ef07f9e21a549acbf778
Jiří Martínek <87183746+jirkamartinekk@users.noreply.github.com>
2024-04-24 10:30:30 +0200

    1.5.0

--- game.cpp
+++ game.cpp
@@ -2,19 +2,22 @@
 #include <iostream>
 #include <string.h>
 #include <conio.h>
+#include <time.h> //knihovna casu
 
 #define MAX 8
+#define CASKONCE 20 
 
 int poziceX = 0;
 int poziceY = 0;
 int zivoty = 3;
 char hraciPole[MAX][MAX];
-
+unsigned long cas, casAktualni, casSpusteniHry; //unsigned =velke cele cislo; long = ma dvounasobnou velikost bytu
+	//cas v pocitaci se pocita od 1.1.1970 00:00:00
 void nactiHraciPole() {
 
 	int sloupec, radek;
 
-	for (radek = 0; radek < MAX; radek++){
+	for (radek = 0; radek < MAX; radek++) {
 
 		for (sloupec = 0; sloupec < MAX; sloupec++)
 			hraciPole[radek][sloupec] = ' ';
@@ -30,7 +33,7 @@ int nactiHraciPoleZeSouboru() {
 	int sloupec, radek, znak;
 
 	FILE* soubor;
-	if (soubor = fopen("H:\\Seminář\\Cčko\\24.01.2024\\sachovniceSeCtenimZeSouboru\\sachovnice\\maps\\02.txt", "r")) {
+	if (soubor = fopen("H:\\Seminář\\Cčko\\31.01.2024\\sachovnice4.0\\maps\\01.txt", "r")) {
 
 		for (radek = 0; radek < MAX; radek++) {
 
@@ -51,38 +54,41 @@ int nactiHraciPoleZeSouboru() {
 	return 0;
 }
 
-	void vypisSachovnici() {
-		int sloupec, radek;
+void vypisSachovnici() {
+	int sloupec, radek;
 
-		system("cls"); //vycisti obrazovku pri dalsim cyklu
+	system("cls"); //vycisti obrazovku pri dalsim cyklu
 
-		printf("Pouzij klavesy W,A,S,D,Q,E,Z,C pro pohyb:");
-		printf("\n");
-		printf("Stiskni klavesu L pro ukonceni:");
-		printf("\n");
-		printf("----------------------------------------------");
-		printf("\nZivoty: %d", zivoty);
-		printf("\n");
-		printf("----------------------------------------------\n");
+	printf("Pouzij klavesy W,A,S,D,Q,E,Z,C pro pohyb:");
+	printf("\n");
+	printf("Stiskni klavesu L pro ukonceni:");
+	printf("\n");
+	cas = time(0);
+	printf("----------------------------------------------");
+	printf("\nZivoty: %d", zivoty);
+	printf("                             ");
+	printf("Cas: %u", CASKONCE - (casAktualni - casSpusteniHry)); //%u for unsigned
+	printf("\n");
+	printf("----------------------------------------------\n");
 
-		for (radek = 0; radek < MAX; radek++) {
-			for (sloupec = 0; sloupec < MAX; sloupec++)
-				printf("+-");
-			printf("+\n");
-			for (sloupec = 0; sloupec < MAX; sloupec++)
-				if (sloupec == poziceX && radek == poziceY)
-					printf("|*");
-				else if (sloupec == MAX - 1 && radek == MAX - 1)
-					printf("|C");
-				else
-					printf("|%c", hraciPole[radek][sloupec]);
-			printf("|\n");
-		}
+	for (radek = 0; radek < MAX; radek++) {
 		for (sloupec = 0; sloupec < MAX; sloupec++)
 			printf("+-");
 		printf("+\n");
-		return;
+		for (sloupec = 0; sloupec < MAX; sloupec++)
+			if (sloupec == poziceX && radek == poziceY)
+				printf("|*");
+			else if (sloupec == MAX - 1 && radek == MAX - 1)
+				printf("|C");
+			else
+				printf("|%c", hraciPole[radek][sloupec]);
+		printf("|\n");
 	}
+	for (sloupec = 0; sloupec < MAX; sloupec++)
+		printf("+-");
+	printf("+\n");
+	return;
+}
 
 
 int main()
@@ -94,6 +100,8 @@ int main()
 		return 0;
 	}
 
+	casSpusteniHry = time(0); //ulozeni casu startu
+
 	vypisSachovnici();
 
 	do {
@@ -102,21 +110,22 @@ int main()
 			if (znak == 224) znak = _getch();
 			switch (znak) {
 			case 75:
-			case 'a': if (poziceX > 0) poziceX--;
+			case 'a': if (poziceX > 0 && (hraciPole[poziceY][poziceX-1] != '&')) poziceX--;
 					else zivoty--;
 				break;
 			case 77:
-			case 'd': if (poziceX < (MAX - 1)) poziceX++;
+			case 'd': if (poziceX < (MAX - 1) && (hraciPole[poziceY][poziceX+1] != '&')) poziceX++;
 					else zivoty--;
 				break;
 			case 72:
-			case 'w': if (poziceY > 0) poziceY--;
+			case 'w': if (poziceY > 0 && (hraciPole[poziceY-1][poziceX] != '&')) poziceY--;
 					else zivoty--;
 				break;
 			case 80:
-			case 's': if (poziceY < (MAX - 1))poziceY++;
+			case 's': if (poziceY < (MAX - 1) && (hraciPole[poziceY+1][poziceX] != '&'))poziceY++;
 					else zivoty--;
 				break;
+			//diagonalni pohyb
 			case 'q':
 				if (poziceX > 0 && poziceY > 0) poziceX--, poziceY--;
 				else zivoty--;
@@ -137,14 +146,19 @@ int main()
 			}
 			vypisSachovnici();
 		}
+		casAktualni = time(0);
+		if ((casAktualni - cas) > 0)
+		{
+			vypisSachovnici();
+		}
 
-	} while (znak != 'l' && !(poziceX == (MAX - 1) && (poziceY == MAX - 1)) && !zivoty == 0);
+	} while (znak != 'l' && !(poziceX == (MAX - 1) && (poziceY == MAX - 1)) && !zivoty == 0 && (CASKONCE-(casAktualni-casSpusteniHry)));
 	if (poziceX == (MAX - 1) && (poziceY == MAX - 1)) {
 		printf("----------------------------------------------\n");
 		printf("Gratuluji, kavalire! Mas u me svacinu...\n");
 		printf("----------------------------------------------\n");
 	}
-	else{
+	else {
 		printf("------------------------------\n");
 		printf("Chcipl jsi na zivoty nebo jsi to leavnul, srabe!\n");
 		printf("------------------------------\n");
